e4cbddc72626c9e39a68de8d4628b451f9de7578
sebastian_sinz02 <sebastian.sinz@outlook.com>
2022-10-06 15:25:03 +0200

    Addieren

---
+++ src/Algodat/Addieren.java
@@ -0,0 +1,21 @@
+package Algodat;
+
+import java.util.Scanner;
+
+public class Addieren {
+    public static void main(String[] args) {
+
+        Scanner Number = new Scanner(System.in);
+        String addierteNumber;
+
+        System.out.println("Enter first number");
+        addierteNumber = Number.nextLine();
+
+        System.out.println("Enter second number");
+        addierteNumber = Number.nextLine();
+
+
+        System.out.println("first Number  is = " + addierteNumber);
+
+    }
+}
