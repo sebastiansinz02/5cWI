593149f099b6c5d6f874dc3aa9e4b0580edf639c
sebastian_sinz02 <sebastian.sinz@outlook.com>
2022-11-18 11:42:19 +0100

    update 18.11.2022 LinkedList finished Stack finished Queue finished

---
+++ src/list/Stack/SMain.java
@@ -0,0 +1,14 @@
+package list.Stack;
+
+public class SMain {
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(4);
+        s.push(5);
+        s.push(6);
+        System.out.println(s.pop());
+        System.out.println(s.peak());
+    }
+
+
+}
---
+++ src/list/Stack/Stack.java
@@ -0,0 +1,42 @@
+package list.Stack;
+import list.HTLStack;
+import list.Node;
+
+public class Stack implements HTLStack {
+
+    int length = 0;
+    Node top = null;
+
+    @Override
+    public void push(int value) {
+        Node temp = new Node(value);
+        temp.setNext(top);
+        top = temp;
+        length++;
+    }
+
+    @Override
+    public int pop() {
+        if(top==null){
+            System.out.println("----------------------");
+            System.out.println("No Value");
+            System.out.println("----------------------");
+            return -1;
+        }
+        Node node = top;
+        top = top.getNext();
+        length--;
+        return node.getValue();
+    }
+
+    @Override
+    public int peak() {
+        if(top==null){
+            System.out.println("----------------------");
+            System.out.println("No Value");
+            System.out.println("----------------------");
+            return -1;
+        }
+        return top.getValue();
+    }
+}
